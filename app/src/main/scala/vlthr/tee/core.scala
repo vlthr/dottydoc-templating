6b296404ce8d6f25a8ae5ffaa2cc5122175baa7e
Valthor Halldorsson <halldorsson.v@gmail.com>
2017-06-20 12:31:06 +0200

    cleanup and formatting

--- app/src/main/scala/vlthr/tee/core.scala
+++ app/src/main/scala/vlthr/tee/core.scala
@@ -2,24 +2,74 @@ package vlthr.tee.core
 
 case class ParseContext(begin: Int, end: Int, template: Template) {}
 
-class EvalContext {
+trait EvalContext {
   def mappings: Map[String, Value] = ???
   def parentScope: EvalContext = ???
   def lookup(s: String): Value = ???
 }
 
-abstract class Expr {
+abstract trait Node {
+  def render()(implicit evalContext: EvalContext): String = ???
+  def parseContext: ParseContext
+}
+
+final case class BlockNode(node: List[Node])(
+    implicit val parseContext: ParseContext)
+    extends Node {}
+
+final case class OutputNode(expr: Expr)(
+    implicit val parseContext: ParseContext)
+    extends Node // {{ 'hello' }}
+
+final case class TextNode(text: String)(
+    implicit val parseContext: ParseContext)
+    extends Node
+
+trait TagNode extends Node {
+  def render(args: List[Expr])(implicit evalContext: EvalContext) = ???
+}
+
+final case class AssignTag(id: String, value: Expr)(
+    implicit val parseContext: ParseContext)
+    extends TagNode
+final case class CaptureTag()(implicit val parseContext: ParseContext)
+    extends TagNode
+final case class CaseTag()(implicit val parseContext: ParseContext)
+    extends TagNode
+final case class CommentTag()(implicit val parseContext: ParseContext)
+    extends TagNode
+final case class CycleTag()(implicit val parseContext: ParseContext)
+    extends TagNode
+final case class ForTag(id: String, expr: Expr, block: Node)(
+    implicit val parseContext: ParseContext)
+    extends TagNode
+final case class BreakTag()(implicit val parseContext: ParseContext)
+    extends TagNode
+final case class ContinueTag()(implicit val parseContext: ParseContext)
+    extends TagNode
+final case class IfTag(condition: Expr, block: Node)(
+    implicit val parseContext: ParseContext)
+    extends TagNode
+final case class IncludeTag()(implicit val parseContext: ParseContext)
+    extends TagNode
+final case class RawTag()(implicit val parseContext: ParseContext)
+    extends TagNode
+final case class UnlessTag()(implicit val parseContext: ParseContext)
+    extends TagNode
+
+abstract trait Expr {
   def parseContext: ParseContext
   def eval()(evalContext: EvalContext): Value = ???
 }
 
-class Filter {
+trait Filter {
   def apply(args: List[Expr]): Value = ???
 }
 
 object Filter {
-  def byName(s: String): Filter = new Filter()
+  def byName(s: String): Filter = new Object() with Filter
 }
+
 final case class AndExpr(left: Expr, right: Expr)(
     implicit val parseContext: ParseContext)
     extends Expr
@@ -60,67 +110,18 @@ final case class DotExpr(indexable: Expr, key: Value)(
     implicit val parseContext: ParseContext)
     extends Expr // l.hello, or l.size (special methods)
 
-abstract class Node {
-  def render()(implicit evalContext: EvalContext): String = ???
-  def parseContext: ParseContext
-}
-
-final case class BlockNode(node: List[Node])(
-    implicit val parseContext: ParseContext)
-    extends Node {}
-
-final case class OutputNode(expr: Expr)(
-    implicit val parseContext: ParseContext)
-    extends Node // {{ 'hello' }}
-
-final case class TextNode(text: String)(
-  implicit val parseContext: ParseContext)
-    extends Node
-
-abstract class TagNode extends Node {
-  def render(args: List[Expr])(implicit evalContext: EvalContext) = ???
-}
-
-final case class AssignTag(id: String, value: Expr)(
-    implicit val parseContext: ParseContext)
-    extends TagNode
-final case class CaptureTag()(implicit val parseContext: ParseContext)
-    extends TagNode
-final case class CaseTag()(implicit val parseContext: ParseContext)
-    extends TagNode
-final case class CommentTag()(implicit val parseContext: ParseContext)
-    extends TagNode
-final case class CycleTag()(implicit val parseContext: ParseContext)
-    extends TagNode
-final case class ForTag(id: String, expr: Expr, block: Node)(
-    implicit val parseContext: ParseContext)
-    extends TagNode
-final case class BreakTag()(implicit val parseContext: ParseContext)
-    extends TagNode
-final case class ContinueTag()(implicit val parseContext: ParseContext)
-    extends TagNode
-final case class IfTag(condition: Expr, block: Node)(
-    implicit val parseContext: ParseContext)
-    extends TagNode
-final case class IncludeTag()(implicit val parseContext: ParseContext)
-    extends TagNode
-final case class RawTag()(implicit val parseContext: ParseContext)
-    extends TagNode
-final case class UnlessTag()(implicit val parseContext: ParseContext)
-    extends TagNode
-
 sealed trait Value {
   def render: String = ???
 }
 
-abstract class IndexedValue extends Value
+sealed trait IndexedValue extends Value
 final case class StringValue(v: String) extends Value
 final case class BooleanValue(v: Boolean) extends Value
 final case class IntValue(v: Int) extends Value
 final case class MapValue(v: Map[String, Value]) extends IndexedValue
 final case class ListValue(v: List[Value]) extends IndexedValue
 
-class Template(body: String, nodes: List[Node]) {
+case class Template(body: String, nodes: List[Node]) {
   def this(path: String) = {
     // read file, populate body/nodes
     this("", Nil)
