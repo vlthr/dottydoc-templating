5a79200f73e1482bf9023d65c212c1b4096ad1a6
Valthor Halldorsson <halldorsson.v@gmail.com>
2017-06-20 18:29:18 +0200

    add parse trees to input-output test suite

--- app/src/main/scala/main.scala
+++ app/src/main/scala/main.scala
@@ -7,8 +7,12 @@ import java.nio.charset.StandardCharsets
 object GenTestExamples extends App {
   args.foreach { input =>
     val basename = input.replaceAll("\\.liquid", "")
-    val ast = Liquid.parse(io.Source.fromFile(input).mkString)
+    val template = io.Source.fromFile(input).mkString
+    val ast = Liquid.parse(template)
+    val parseTree = Liquid.getParseTree(template)
     Files.write(Paths.get(basename + ".ast"),
                 ast.toString.getBytes(StandardCharsets.UTF_8))
+    Files.write(Paths.get(basename + ".parseTree"),
+                parseTree.toString.getBytes(StandardCharsets.UTF_8))
   }
 }
--- test_examples.sh
+++ test_examples.sh
@@ -19,25 +19,38 @@ liquidfiles=(./app/src/test/resources/examples/*.liquid)
 spacesep="${liquidfiles[@]}"
 sbt "genTestExamples $spacesep"
 
-astfiles=(./app/src/test/resources/examples/*.ast)
 # commasep=$(IFS=, ; echo "${astfiles[*]}")
 # scalafmt -i --files $commasep
 if [[ $REGENERATE ]]; then
     echo "Regenerating expected outputs..."
-    for f in ${astfiles[@]}; do
-        echo $f ...
-        cp $f $f-expected
+    for f in ${liquidfiles[@]}; do
+        base="${f%.*}"
+        ast="$base.ast"
+        parseTree="$base.parseTree"
+        echo $base ...
+        cp $ast $ast-expected
+        cp $parseTree $parseTree-expected
     done
 else
     echo "Comparing generated files with expected outputs..."
-    for f in ${astfiles[@]}; do
-        echo -n $f ...
-        d=$(diff $f $f-expected)
+    for f in ${liquidfiles[@]}; do
+        base="${f%.*}"
+        ast="$base.ast"
+        parseTree="$base.parseTree"
+        echo -n $base ...
+        parseTree_diff=$(diff $parseTree $parseTree-expected)
         if [ $? -eq 0 ]; then
-            echo "correct!"
+            echo "parse tree correct!"
         else
-            echo "mismatch!"
-            echo "${d}"
+            echo "parse tree mismatch!"
+            echo "${parseTree_diff}"
+        fi
+        ast_diff=$(diff $ast $ast-expected)
+        if [ $? -eq 0 ]; then
+            echo "AST correct!"
+        else
+            echo "AST mismatch!"
+            echo "${ast_diff}"
         fi
     done
 fi
