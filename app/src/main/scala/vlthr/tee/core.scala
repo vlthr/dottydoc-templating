cc1af6f3e6a43c6d0092a347ad6c4034d66ccf46
Valthor Halldorsson <halldorsson.v@gmail.com>
2017-06-26 10:19:27 +0200

    Revert "simple string based codegen"
    This reverts commit 25fb3649fb29c2827aa5089cb697ad77580d07fe.

--- app/src/main/scala/vlthr/tee/core.scala
+++ app/src/main/scala/vlthr/tee/core.scala
@@ -11,30 +11,22 @@ trait EvalContext {
 abstract trait Node {
   def render()(implicit evalContext: EvalContext): String = ???
   def parseContext: ParseContext
-  def codeGen(): String = ???
 }
 
 final case class BlockNode(node: List[Node])(
     implicit val parseContext: ParseContext)
-    extends Node {
-  override def codeGen(): String = node.map(_.codeGen()).mkString(" +\n")
-}
+    extends Node {}
 
 final case class OutputNode(expr: Expr)(
     implicit val parseContext: ParseContext)
-    extends Node {
-  override def codeGen(): String = s"""{ ${expr.codeGen()} }.toString"""
-}
+    extends Node // {{ 'hello' }}
 
 final case class TextNode(text: String)(
     implicit val parseContext: ParseContext)
-    extends Node {
-  override def codeGen(): String = CodeGenUtil.escape(s"""'$text'""")
-}
+    extends Node
 
 trait TagNode extends Node {
   def render(args: List[Expr])(implicit evalContext: EvalContext) = ???
-  override def codeGen(): String = ""
 }
 
 final case class AssignTag(id: String, value: Expr)(
@@ -67,17 +59,14 @@ final case class UnlessTag()(implicit val parseContext: ParseContext)
 
 abstract trait Expr {
   def parseContext: ParseContext
-  def eval(): Value = ???
-  def codeGen(): String = ???
+  def eval()(evalContext: EvalContext): Value = ???
 }
 
 abstract trait Filter {
-  def apply(expr: Expr, args: List[Expr]): Value
+  def apply(args: List[Expr]): Value = ???
 }
 
-case class NoFilter() extends Filter {
-  def apply(expr: Expr, args: List[Expr]): Value = expr.eval()
-}
+case class NoFilter() extends Filter
 
 object Filter {
   def byName(s: String): Filter = NoFilter()
@@ -109,51 +98,30 @@ final case class GtExpr(left: Expr, right: Expr)(
     extends Expr
 final case class LiteralExpr(value: Value)(
     implicit val parseContext: ParseContext)
-    extends Expr {
-  override def codeGen(): String = value.codeGen()
-}
+    extends Expr
 final case class VariableUseExpr(name: String)(
     implicit val parseContext: ParseContext)
-    extends Expr {
-  override def codeGen(): String = name
-}
+    extends Expr
 final case class FilterExpr(expr: Expr, filter: Filter, args: List[Expr])(
     implicit val parseContext: ParseContext)
-    extends Expr {
-  override def codeGen(): String = expr.codeGen()
-}
+    extends Expr
 final case class IndexExpr(indexable: Expr, key: Expr)(
     implicit val parseContext: ParseContext)
-    extends Expr {
-  override def codeGen(): String = s"${indexable.codeGen()}(${key.codeGen()})"
-}
+    extends Expr // l[0], l['hello'],
 final case class DotExpr(indexable: Expr, key: String)(
     implicit val parseContext: ParseContext)
-    extends Expr {
-  override def codeGen(): String = s"${indexable.codeGen()}.$key"
-}
+    extends Expr // l.hello, or l.size (special methods)
 
 sealed trait Value {
   def render: String = ???
-  def codeGen(): String = ???
 }
 
 sealed trait IndexedValue extends Value
-final case class StringValue(v: String) extends Value {
-  override def codeGen(): String = s""""$v""""
-}
-final case class BooleanValue(v: Boolean) extends Value {
-  override def codeGen(): String = s"$v"
-}
-final case class IntValue(v: Int) extends Value {
-  override def codeGen(): String = s"$v"
-}
-final case class MapValue(v: Map[String, Value]) extends IndexedValue {
-  override def codeGen(): String = s"$v"
-}
-final case class ListValue(v: List[Value]) extends IndexedValue {
-  override def codeGen(): String = s"$v"
-}
+final case class StringValue(v: String) extends Value
+final case class BooleanValue(v: Boolean) extends Value
+final case class IntValue(v: Int) extends Value
+final case class MapValue(v: Map[String, Value]) extends IndexedValue
+final case class ListValue(v: List[Value]) extends IndexedValue
 
 case class Template(body: String, nodes: List[Node]) {
   def this(path: String) = {
@@ -166,21 +134,4 @@ case class Template(body: String, nodes: List[Node]) {
     body
   }
 
-}
-
-object CodeGenUtil {
-  def escape(s: String): String = s.flatMap(escapedChar)
-
-  def escapedChar(ch: Char): String = ch match {
-    case '\b' => "\\b"
-    case '\t' => "\\t"
-    case '\n' => "\\n"
-    case '\f' => "\\f"
-    case '\r' => "\\r"
-    case '"'  => "\\\""
-    case '\'' => "\\\'"
-    case '\\' => "\\\\"
-    case _    => if (ch.isControl) "\\0" + Integer.toOctalString(ch.toInt)
-                 else              String.valueOf(ch)
-  }
 }
